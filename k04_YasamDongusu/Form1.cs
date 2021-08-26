f875e0785d6d76956f50b9b0b65430ea9d35c046
cigdemturkmen <cigdemturkmen57@outlook.com>
2021-08-26 13:33:24 +0300

    yasamdong

---
+++ k04_YasamDongusu/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace k04_YasamDongusu
+{
+    public partial class Form1 : Form
+    {
+        //constructor (yapıcı metod)
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ k04_YasamDongusu/YasamDongusu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace k04_YasamDongusu
+{
+    public partial class YasamDongusu : Form
+    {
+        // scope : {} arasındaki alanlar
+        int GlobalDegisken = 100;
+        public YasamDongusu() //constructor
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GlobalDegisken += 5;
+            txtDeger.Text = GlobalDegisken.ToString();
+        }
+    }
+}
