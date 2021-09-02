ff9f4baba1e39db54e3ad8bf01c0be5b60af8bca
cigdemturkmen <cigdemturkmen57@outlook.com>
2021-09-02 14:03:04 +0300

    Donguler

---
+++ k07_Donguler/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace k07_Donguler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstSonuc = new System.Windows.Forms.ListBox();
+            this.btn1den100e = new System.Windows.Forms.Button();
+            this.btn100den1e = new System.Windows.Forms.Button();
+            this.btn10dan50ye3er = new System.Windows.Forms.Button();
+            this.btnAdanZye = new System.Windows.Forms.Button();
+            this.btn20den100ecift = new System.Windows.Forms.Button();
+            this.btn1den11eToplam = new System.Windows.Forms.Button();
+            this.btn10la30arasiTekSayilarTopla = new System.Windows.Forms.Button();
+            this.btn2000ve2020 = new System.Windows.Forms.Button();
+            this.cmbYears = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSecilenYil = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRastgele = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstSonuc
+            // 
+            this.lstSonuc.FormattingEnabled = true;
+            this.lstSonuc.Location = new System.Drawing.Point(476, 17);
+            this.lstSonuc.Name = "lstSonuc";
+            this.lstSonuc.Size = new System.Drawing.Size(202, 407);
+            this.lstSonuc.TabIndex = 3;
+            // 
+            // btn1den100e
+            // 
+            this.btn1den100e.Location = new System.Drawing.Point(12, 17);
+            this.btn1den100e.Name = "btn1den100e";
+            this.btn1den100e.Size = new System.Drawing.Size(199, 23);
+            this.btn1den100e.TabIndex = 4;
+            this.btn1den100e.Text = "1den 100e kadar sayıları yazdır";
+            this.btn1den100e.UseVisualStyleBackColor = true;
+            this.btn1den100e.Click += new System.EventHandler(this.btn1den100e_Click);
+            // 
+            // btn100den1e
+            // 
+            this.btn100den1e.Location = new System.Drawing.Point(12, 61);
+            this.btn100den1e.Name = "btn100den1e";
+            this.btn100den1e.Size = new System.Drawing.Size(199, 23);
+            this.btn100den1e.TabIndex = 5;
+            this.btn100den1e.Text = "100den 1e geri sayım ";
+            this.btn100den1e.UseVisualStyleBackColor = true;
+            this.btn100den1e.Click += new System.EventHandler(this.btn100den1e_Click);
+            // 
+            // btn10dan50ye3er
+            // 
+            this.btn10dan50ye3er.Location = new System.Drawing.Point(12, 103);
+            this.btn10dan50ye3er.Name = "btn10dan50ye3er";
+            this.btn10dan50ye3er.Size = new System.Drawing.Size(199, 23);
+            this.btn10dan50ye3er.TabIndex = 6;
+            this.btn10dan50ye3er.Text = "10dan 50ye 3er artarak";
+            this.btn10dan50ye3er.UseVisualStyleBackColor = true;
+            this.btn10dan50ye3er.Click += new System.EventHandler(this.btn10dan50ye3er_Click);
+            // 
+            // btnAdanZye
+            // 
+            this.btnAdanZye.Location = new System.Drawing.Point(12, 143);
+            this.btnAdanZye.Name = "btnAdanZye";
+            this.btnAdanZye.Size = new System.Drawing.Size(199, 23);
+            this.btnAdanZye.TabIndex = 7;
+            this.btnAdanZye.Text = "Adan Zye harfler";
+            this.btnAdanZye.UseVisualStyleBackColor = true;
+            this.btnAdanZye.Click += new System.EventHandler(this.btnAdanZye_Click);
+            // 
+            // btn20den100ecift
+            // 
+            this.btn20den100ecift.Location = new System.Drawing.Point(12, 182);
+            this.btn20den100ecift.Name = "btn20den100ecift";
+            this.btn20den100ecift.Size = new System.Drawing.Size(199, 23);
+            this.btn20den100ecift.TabIndex = 8;
+            this.btn20den100ecift.Text = "20\'den 100\'e çift sayılar";
+            this.btn20den100ecift.UseVisualStyleBackColor = true;
+            this.btn20den100ecift.Click += new System.EventHandler(this.btn20den100ecift_Click);
+            // 
+            // btn1den11eToplam
+            // 
+            this.btn1den11eToplam.Location = new System.Drawing.Point(12, 222);
+            this.btn1den11eToplam.Name = "btn1den11eToplam";
+            this.btn1den11eToplam.Size = new System.Drawing.Size(199, 23);
+            this.btn1den11eToplam.TabIndex = 9;
+            this.btn1den11eToplam.Text = "1le 11 arasındaki sayılar toplamı";
+            this.btn1den11eToplam.UseVisualStyleBackColor = true;
+            this.btn1den11eToplam.Click += new System.EventHandler(this.btn1den11eToplam_Click);
+            // 
+            // btn10la30arasiTekSayilarTopla
+            // 
+            this.btn10la30arasiTekSayilarTopla.Location = new System.Drawing.Point(12, 262);
+            this.btn10la30arasiTekSayilarTopla.Name = "btn10la30arasiTekSayilarTopla";
+            this.btn10la30arasiTekSayilarTopla.Size = new System.Drawing.Size(199, 23);
+            this.btn10la30arasiTekSayilarTopla.TabIndex = 10;
+            this.btn10la30arasiTekSayilarTopla.Text = "10la 30 arasında tek sayılar toplamı";
+            this.btn10la30arasiTekSayilarTopla.UseVisualStyleBackColor = true;
+            this.btn10la30arasiTekSayilarTopla.Click += new System.EventHandler(this.btn10la30arasiTekSayilarTopla_Click);
+            // 
+            // btn2000ve2020
+            // 
+            this.btn2000ve2020.Location = new System.Drawing.Point(12, 303);
+            this.btn2000ve2020.Name = "btn2000ve2020";
+            this.btn2000ve2020.Size = new System.Drawing.Size(199, 23);
+            this.btn2000ve2020.TabIndex = 11;
+            this.btn2000ve2020.Text = "2000 ile 2020 arasınddaki yıllar";
+            this.btn2000ve2020.UseVisualStyleBackColor = true;
+            this.btn2000ve2020.Click += new System.EventHandler(this.btn2000ve2020_Click);
+            // 
+            // cmbYears
+            // 
+            this.cmbYears.FormattingEnabled = true;
+            this.cmbYears.Location = new System.Drawing.Point(291, 19);
+            this.cmbYears.Name = "cmbYears";
+            this.cmbYears.Size = new System.Drawing.Size(121, 21);
+            this.cmbYears.TabIndex = 12;
+            this.cmbYears.SelectedIndexChanged += new System.EventHandler(this.cmbYears_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Bir yıl seçiniz:";
+            // 
+            // txtSecilenYil
+            // 
+            this.txtSecilenYil.Location = new System.Drawing.Point(353, 64);
+            this.txtSecilenYil.Name = "txtSecilenYil";
+            this.txtSecilenYil.Size = new System.Drawing.Size(59, 20);
+            this.txtSecilenYil.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Seçilen Yıl:";
+            // 
+            // btnRastgele
+            // 
+            this.btnRastgele.Location = new System.Drawing.Point(308, 172);
+            this.btnRastgele.Name = "btnRastgele";
+            this.btnRastgele.Size = new System.Drawing.Size(75, 23);
+            this.btnRastgele.TabIndex = 16;
+            this.btnRastgele.Text = "Rastgele sayı üret";
+            this.btnRastgele.UseVisualStyleBackColor = true;
+            this.btnRastgele.Click += new System.EventHandler(this.btnRastgele_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRastgele);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSecilenYil);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbYears);
+            this.Controls.Add(this.btn2000ve2020);
+            this.Controls.Add(this.btn10la30arasiTekSayilarTopla);
+            this.Controls.Add(this.btn1den11eToplam);
+            this.Controls.Add(this.btn20den100ecift);
+            this.Controls.Add(this.btnAdanZye);
+            this.Controls.Add(this.btn10dan50ye3er);
+            this.Controls.Add(this.btn100den1e);
+            this.Controls.Add(this.btn1den100e);
+            this.Controls.Add(this.lstSonuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstSonuc;
+        private System.Windows.Forms.Button btn1den100e;
+        private System.Windows.Forms.Button btn100den1e;
+        private System.Windows.Forms.Button btn10dan50ye3er;
+        private System.Windows.Forms.Button btnAdanZye;
+        private System.Windows.Forms.Button btn20den100ecift;
+        private System.Windows.Forms.Button btn1den11eToplam;
+        private System.Windows.Forms.Button btn10la30arasiTekSayilarTopla;
+        private System.Windows.Forms.Button btn2000ve2020;
+        private System.Windows.Forms.ComboBox cmbYears;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSecilenYil;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRastgele;
+    }
+}
+
---
+++ k07_Donguler/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace k07_Donguler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btn1den100e_Click(object sender, EventArgs e)
+        {
+            //1'den 100'e kadar sayıları listbox'da göster.
+            for (int i = 1; i < 101; i++)
+            {
+                lstSonuc.Items.Add(i);
+            }
+        }
+
+
+        private void btn100den1e_Click(object sender, EventArgs e)
+        {
+            //100'den 1'e geri sayım yazdır lisbox'a.
+            for (int i = 100; i > 0; i--)
+            {
+                lstSonuc.Items.Add(i);
+            }
+        }
+
+
+        private void btn10dan50ye3er_Click(object sender, EventArgs e)
+        {
+            //10dan 50ye kadar 3er arttırarak yazdır.
+            for (int i = 10; i < 51; i += 3)
+            {
+                lstSonuc.Items.Add(i);
+            }
+        }
+
+
+        private void btnAdanZye_Click(object sender, EventArgs e)
+        {
+            //A'dan Z'ye kadar harfleri yazdır.
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                lstSonuc.Items.Add($"{i} ASCII -> {Convert.ToInt32(i)}");
+            }
+        }
+
+
+        private void btn20den100ecift_Click(object sender, EventArgs e)
+        {
+            //20den 100e kadar çift sayıları yazdır. i +=2'yi kullanmadan yap bunu.
+            for (int i = 20; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    lstSonuc.Items.Add(i);
+                }
+            }
+        }
+
+
+        private void btn1den11eToplam_Click(object sender, EventArgs e)
+        {
+            //[1-11] arasındaki sayilarin toplamini messageboxta göster.
+            int toplam = 0;
+
+            for (int i = 1; i < 12; i++)
+            {
+                toplam = toplam + i;
+            }
+
+            MessageBox.Show(toplam.ToString());
+        }
+
+        private void btn10la30arasiTekSayilarTopla_Click(object sender, EventArgs e)
+        {
+            //10la 30 arasındaki tek sayıların toplamını bulunuz. yine artış miktarı değil if ile yapınız.
+            int toplam = 0;
+            for (int i = 10; i < 31; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    lstSonuc.Items.Add(i);
+                    //toplam = toplam + i;
+                    toplam += i;
+                }
+            }
+
+            MessageBox.Show(toplam.ToString());
+        }
+
+
+        //2000 ile 2020 arasındaki yılları comboboxa yazdıralım. sonrasında comboboxtan bir yıl seçelim ve textboxta gösterelim.
+        private void btn2000ve2020_Click(object sender, EventArgs e)
+        {
+            for (int i = 2000; i < 2021; i++)
+            {
+                cmbYears.Items.Add(i); //boxing
+            }
+        }
+
+        private void cmbYears_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //comboboxtan veri seçimi yapmak için.
+            if (cmbYears.SelectedIndex != 100) //selected index değeri herhangi bir seçim olmazsa otomatik olarak -1 değerini alır. veya cmbYears.SelectedIndex != null --NULL CHECK
+                //null değerini convert edemezsiniz.
+            {
+                var secilenYil = cmbYears.SelectedItem;
+                txtSecilenYil.Text = secilenYil.ToString();
+                //var secilenYil = Convert.ToDouble(cmbYears.SelectedItem); //unboxing
+            }
+
+
+        }
+
+        private void btnRastgele_Click(object sender, EventArgs e)
+        {
+            //rastgele sayılar üretip, 10 tanesini listboxta göster. aynı sayılar gelmesin.
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int rastgeleSayi = rnd.Next(1, 11);
+                if (lstSonuc.Items.Contains(rastgeleSayi) == false)
+                {
+
+                }
+                lstSonuc.Items.Add(rastgeleSayi);
+            }
+
+           
+        }
+    }
+}
