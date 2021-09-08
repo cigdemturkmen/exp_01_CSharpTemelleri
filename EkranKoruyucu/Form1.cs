6888ec6435f5b0c50fe8fe92fc8fc54722f9fdf0
cigdemturkmen <cigdemturkmen57@outlook.com>
2021-09-08 14:01:05 +0300

    password generator ve ekran koruyucu

---
+++ EkranKoruyucu/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EkranKoruyucu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //form içindekiler yüklendikten sonra resmin rastgele olarak yer değiştirmesini istiyoruz.
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //timerın sadece 1 tane eventi vardır. o da bu.tick eventi
+            Random rnd = new Random();
+
+            var formunGenisligi = this.Width;
+            var yukseklik = this.Height;
+
+            var rastgele1 = rnd.Next(0, formunGenisligi);
+            var rastgele2 = rnd.Next(0, yukseklik);
+
+            pictureBox1.Left = rastgele1;
+            pictureBox1.Top = rastgele2;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //koruyucu ekrandan çıkmak için click eventi.
+            Application.Exit();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //koruyucu ekrandan çıkmak için klavyede bir şeye bas.
+            Application.Exit();
+        }
+
+        //alt + tab ekran koruyucudan çıkar
+    }
+}
--- k09_ArraySinifi/PasswordGeneratorArray.cs
+++ k09_ArraySinifi/PasswordGeneratorArray.cs
@@ -26,7 +26,89 @@ namespace k09_ArraySinifi
         {
             var karakterSayisi = Convert.ToInt32(txtKarakterSayisi);
 
-            
+            var buyukHarfler = new char[0];
+            var kucukHarfler = new char[0];
+            var rakamlar = new char[0];
+            var semboller = new char[0];
+
+            //dizi içleri for ile doldurulabilir
+
+            for (char i = 'A'; i <= 'Z'; i++)
+           
+            {
+                Array.Resize(ref buyukHarfler, buyukHarfler.Length + 1);
+                buyukHarfler[buyukHarfler.Length - 1] = i;
+            }
+
+
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                Array.Resize(ref kucukHarfler, kucukHarfler.Length + 1);
+                kucukHarfler[kucukHarfler.Length - 1] = i;
+            }
+
+            for (char i = '0'; i <= '9'; i++)
+            {
+                Array.Resize(ref rakamlar, rakamlar.Length + 1);
+                rakamlar[rakamlar.Length - 1] = i;
+            }
+
+            for (int i = 33; i <= 47; i++)
+            {
+                Array.Resize(ref semboller, semboller.Length + 1);
+                semboller[semboller.Length - 1] = Convert.ToChar(i);
+            }
+
+            Random rnd = new Random();
+            var hangiDizidenSecimYapilsin = rnd.Next(0, 4); //0, 1, 2, 3
+
+            //if (hangiDizidenSecimYapilsin == 0)
+            //{
+            //    //buyuk harflerden rastgele seçip şifrenin içine ekleyeceğiz.
+            //}
+
+            var sifre = "";
+            switch (hangiDizidenSecimYapilsin)
+            {
+                case 0:
+                    //buyuklerden seç
+                    if (cbBuyukHarf.Checked)
+                    {
+                        var rastgeleIndex = rnd.Next(0, buyukHarfler.Length);
+                        var secilenHarf = buyukHarfler[rastgeleIndex];
+                        sifre += secilenHarf;
+                    }
+                    break;
+                case 1:
+                    //kucuklerden sec
+                    if (cbKucukHarf.Checked)
+                    {
+                        var rastgeleIndex = rnd.Next(0, kucukHarfler.Length);
+                        var secilenHarf = kucukHarfler[rastgeleIndex];
+                        sifre += secilenHarf;
+                    }
+                    break;
+                case 2:
+                    //rakamlardan sec
+                    if (cbRakam.Checked)
+                    {
+                        var rastgeleIndex = rnd.Next(0, rakamlar.Length);
+                        var secilenHarf = rakamlar[rastgeleIndex];
+                        sifre += secilenHarf;
+                    }
+                    break;
+                case 3:
+                    //sembollerden sec
+                    if (cbSembol.Checked)
+                    {
+                        var rastgeleIndex = rnd.Next(0, semboller.Length);
+                        var secilenHarf = semboller[rastgeleIndex];
+                        sifre += secilenHarf;
+                    }
+                    break;
+
+                //default:
+            }
         }
     }
 }
