5653b91fdc09098b7d681d5c93c4ea4e59fdf0c9
cigdemturkmen <cigdemturkmen57@outlook.com>
2021-09-09 14:00:28 +0300

    Math ve String metodları2

---
+++ k10_HazirFonksiyonlar/StringFonksiyonlar.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace k10_HazirFonksiyonlar
+{
+    partial class StringFonksiyonlar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.btnStartWithEndWith = new System.Windows.Forms.Button();
+            this.btnIndexOf = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnToLowerToUpper = new System.Windows.Forms.Button();
+            this.btnSubstring = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.btnToCharArray = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.btnTrim = new System.Windows.Forms.Button();
+            this.btnMailOlustur = new System.Windows.Forms.Button();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(67, 0);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(177, 40);
+            this.btnCompare.TabIndex = 0;
+            this.btnCompare.Text = "Contains";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // btnStartWithEndWith
+            // 
+            this.btnStartWithEndWith.Location = new System.Drawing.Point(67, 40);
+            this.btnStartWithEndWith.Name = "btnStartWithEndWith";
+            this.btnStartWithEndWith.Size = new System.Drawing.Size(177, 40);
+            this.btnStartWithEndWith.TabIndex = 1;
+            this.btnStartWithEndWith.Text = "StartsWithEndsWith";
+            this.btnStartWithEndWith.UseVisualStyleBackColor = true;
+            this.btnStartWithEndWith.Click += new System.EventHandler(this.btnStartWithEndWith_Click);
+            // 
+            // btnIndexOf
+            // 
+            this.btnIndexOf.Location = new System.Drawing.Point(67, 80);
+            this.btnIndexOf.Name = "btnIndexOf";
+            this.btnIndexOf.Size = new System.Drawing.Size(177, 40);
+            this.btnIndexOf.TabIndex = 2;
+            this.btnIndexOf.Text = "IndexOf/LastIndexOf";
+            this.btnIndexOf.UseVisualStyleBackColor = true;
+            this.btnIndexOf.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(67, 120);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(177, 40);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove()";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(67, 160);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(177, 40);
+            this.btnReplace.TabIndex = 4;
+            this.btnReplace.Text = "Replace()";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(67, 200);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(177, 40);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "Insert()";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnToLowerToUpper
+            // 
+            this.btnToLowerToUpper.Location = new System.Drawing.Point(67, 240);
+            this.btnToLowerToUpper.Name = "btnToLowerToUpper";
+            this.btnToLowerToUpper.Size = new System.Drawing.Size(177, 40);
+            this.btnToLowerToUpper.TabIndex = 6;
+            this.btnToLowerToUpper.Text = "ToLower()/ToUpper()";
+            this.btnToLowerToUpper.UseVisualStyleBackColor = true;
+            this.btnToLowerToUpper.Click += new System.EventHandler(this.btnToLowerToUpper_Click);
+            // 
+            // btnSubstring
+            // 
+            this.btnSubstring.Location = new System.Drawing.Point(67, 280);
+            this.btnSubstring.Name = "btnSubstring";
+            this.btnSubstring.Size = new System.Drawing.Size(177, 40);
+            this.btnSubstring.TabIndex = 7;
+            this.btnSubstring.Text = "Substring()";
+            this.btnSubstring.UseVisualStyleBackColor = true;
+            this.btnSubstring.Click += new System.EventHandler(this.btnSubstring_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(67, 320);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(177, 40);
+            this.btnSplit.TabIndex = 8;
+            this.btnSplit.Text = "Split()";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // btnToCharArray
+            // 
+            this.btnToCharArray.Location = new System.Drawing.Point(67, 360);
+            this.btnToCharArray.Name = "btnToCharArray";
+            this.btnToCharArray.Size = new System.Drawing.Size(177, 40);
+            this.btnToCharArray.TabIndex = 9;
+            this.btnToCharArray.Text = "ToCharArray()";
+            this.btnToCharArray.UseVisualStyleBackColor = true;
+            this.btnToCharArray.Click += new System.EventHandler(this.btnToCharArray_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(67, 400);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(177, 40);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Compare";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // btnTrim
+            // 
+            this.btnTrim.Location = new System.Drawing.Point(67, 440);
+            this.btnTrim.Name = "btnTrim";
+            this.btnTrim.Size = new System.Drawing.Size(177, 40);
+            this.btnTrim.TabIndex = 11;
+            this.btnTrim.Text = "Trim()";
+            this.btnTrim.UseVisualStyleBackColor = true;
+            this.btnTrim.Click += new System.EventHandler(this.btnTrim_Click);
+            // 
+            // btnMailOlustur
+            // 
+            this.btnMailOlustur.Location = new System.Drawing.Point(469, 46);
+            this.btnMailOlustur.Name = "btnMailOlustur";
+            this.btnMailOlustur.Size = new System.Drawing.Size(158, 23);
+            this.btnMailOlustur.TabIndex = 12;
+            this.btnMailOlustur.Text = "Mail oluştur";
+            this.btnMailOlustur.UseVisualStyleBackColor = true;
+            this.btnMailOlustur.Click += new System.EventHandler(this.btnMailOlustur_Click);
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(469, 20);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(158, 20);
+            this.txtAdSoyad.TabIndex = 13;
+            // 
+            // StringFonksiyonlar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.btnMailOlustur);
+            this.Controls.Add(this.btnTrim);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.btnToCharArray);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.btnSubstring);
+            this.Controls.Add(this.btnToLowerToUpper);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnIndexOf);
+            this.Controls.Add(this.btnStartWithEndWith);
+            this.Controls.Add(this.btnCompare);
+            this.Name = "StringFonksiyonlar";
+            this.Text = "StringFonksiyonlar";
+            this.Load += new System.EventHandler(this.StringFonksiyonlar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Button btnStartWithEndWith;
+        private System.Windows.Forms.Button btnIndexOf;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnToLowerToUpper;
+        private System.Windows.Forms.Button btnSubstring;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.Button btnToCharArray;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button btnTrim;
+        private System.Windows.Forms.Button btnMailOlustur;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+    }
+}
